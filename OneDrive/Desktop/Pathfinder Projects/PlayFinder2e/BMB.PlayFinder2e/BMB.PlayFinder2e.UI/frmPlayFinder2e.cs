517747c13ed8aec853808dd82f31ef24413c527a
Brandon Bullock <300088062@fvtc.edu>
2026-02-24 20:54:19 -0600

    bulk commit

---
+++ OneDrive/Desktop/Pathfinder Projects/PlayFinder2e/BMB.PlayFinder2e/BMB.PlayFinder2e.UI/frmPlayFinder2e.cs
@@ -0,0 +1,10 @@
+namespace BMB.PlayFinder2e.UI
+{
+    public partial class frmPlayFinder2e : Form
+    {
+        public frmPlayFinder2e()
+        {
+            InitializeComponent();
+        }
+    }
+}
